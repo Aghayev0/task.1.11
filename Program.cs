ec8528316c1b51dde41a2b3f7a8e570cd2a11eaa
Aghayev0 <tu6k6wzh2@code.edu.az>
2022-11-01 21:42:55 +0400

    Her vaxtiniz xeyir olsun

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//int[] number = { 12, 34, 56, 78, 11, 10 };
+//int reqem = number[0];
+//for (int i = 0; i < number.Length; i++)
+//{
+//    if (number[i] < reqem)
+//        reqem = number[i];
+//}
+//Console.WriteLine(reqem);
+
+int[] number = { 1, 4, 56, 7, 11, 10, 8 };
+int reqem = 0;
+for (int i = 0; i < number.Length; i++)
+{
+    if (number[i] < 10)
+        reqem = reqem + 1;
+}
+Console.WriteLine(reqem);
